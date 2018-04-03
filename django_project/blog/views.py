c34a872193abe0389cca8b2ce2589658eafa7c83
tz2614 <tony_zheng35@hotmail.com>
2018-04-03 17:52:50 +0100

    git repo for djangointro

---
+++ django_project/blog/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+import datetime
+
+def index(request):
+	return HttpResponse("Hello Django")
+
+def today_is(request):
+	now = datetime.datetime.now()
+	html = "<html><body>Current date and time: {0}</body></html>".format(now)
+	return HttpResponse(html)
