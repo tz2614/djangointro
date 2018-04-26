2eab957f3d36b106cc08254220bc3874f6ff7640
tz2614 <tony_zheng35@hotmail.com>
2018-04-26 17:24:16 +0100

    django 1.10 intro update

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response, render
 import datetime
-
+from django.conf import settings
 def index(request):
 	return HttpResponse("Hello Django")
 
 def today_is(request):
 	now = datetime.datetime.now()
-	html = "<html><body>Current date and time: {0}</body></html>".format(now)
-	return HttpResponse(html)
+	return render(request, 'blog/datetime.html', {
+									'now': now, 
+									'template_name': 'blog/nav.html',
+									'base_dir': settings.BASE_DIR}
+								)
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -55,7 +54,9 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
